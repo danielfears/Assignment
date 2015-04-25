b8c10cc906525f5a5606e250e7fcaf4d4008f9e9
Daniel Fears <danieljfears@gmail.com>
2015-04-25 23:20:49 +0100

    Working through API part 2

---
+++ app/src/main/java/danieljfears/assignment/FlickrResponse.java
@@ -0,0 +1,34 @@
+package danieljfears.assignment;
+
+import java.util.List;
+
+public class FlickrResponse {
+
+    private FlickrPhotos photos;
+
+    public FlickrPhotos getPhotos() {return photos;}
+
+
+        public class FlickrPhotos {
+        private List<FlickrPhoto> photo;
+
+        public List<FlickrPhoto> getPhoto() {return photo;
+        }
+
+        public class FlickrPhoto {
+
+            private String title;
+            private String url_o;
+            private String url_m;
+
+            public String getTitle() {return title;}
+            public String getUrl_o() {return url_o;}
+            public String getUrl_m() {return url_m;}
+
+            public String getImageURL() {return getImageURL();}
+
+            }
+        }
+    }
+
+}
