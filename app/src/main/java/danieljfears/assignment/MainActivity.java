dc2a589438a9c84d021476150d9ba6e180a5f83f
Daniel Fears <danieljfears@gmail.com>
2015-05-11 14:51:05 +0100

    App finished with feature and material design

--- app/src/main/java/danieljfears/assignment/MainActivity.java
+++ app/src/main/java/danieljfears/assignment/MainActivity.java
@@ -3,42 +3,40 @@ package danieljfears.assignment;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
 
 public class MainActivity extends ActionBarActivity {
 
     private Button btnLocations;
-    private Button btnSearch;
+    private Button btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLocations = (Button)findViewById(R.id.btnLocations);
-//        btnSearch = (Button)findViewById(R.id.btnSearch);
+        btnLocations = (Button) findViewById(R.id.btnLocations);
 
-
-        btnLocations.setOnClickListener( new View.OnClickListener() {
+        btnLocations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, LocationsActivity.class);
                 startActivity(i);
-                Log.d("FLICKR_APP", "pushed button");
+
+            };
+        });
+
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+
             }
         });
 
-//        btnSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(MainActivity.this, SearchActivity.class);
-//                startActivity(i);
-//                Log.d("FLICKR_APP", "pushed button");
-//            }
-//        });
     }
 }
